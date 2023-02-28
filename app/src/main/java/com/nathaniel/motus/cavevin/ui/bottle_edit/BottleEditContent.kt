284b5cbd19da798fb420dc299893e2ce3c67b595
Nathaniel Motus <nathaniel.motus@laposte.net>
2023-02-28 23:35:01 +0100

    Start migration to Compose Migrate to Material3

--- app/src/main/java/com/nathaniel/motus/cavevin/ui/bottle_edit/BottleEditContent.kt
+++ app/src/main/java/com/nathaniel/motus/cavevin/ui/bottle_edit/BottleEditContent.kt
@@ -2,28 +2,34 @@ package com.nathaniel.motus.cavevin.ui.bottle_edit
 
 import android.content.Intent
 import androidx.compose.foundation.Image
+import androidx.compose.foundation.background
 import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.*
-import androidx.compose.material3.ExperimentalMaterial3Api
-import androidx.compose.material3.OutlinedTextField
-import androidx.compose.material3.Text
+import androidx.compose.foundation.shape.CircleShape
+import androidx.compose.material3.*
 import androidx.compose.runtime.*
 import androidx.compose.runtime.livedata.observeAsState
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.drawBehind
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.graphics.ColorFilter
 import androidx.compose.ui.graphics.ImageBitmap
+import androidx.compose.ui.graphics.Shape
 import androidx.compose.ui.layout.ContentScale
 import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.platform.LocalLayoutDirection
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
 import androidx.core.content.ContentProviderCompat.requireContext
 import androidx.core.content.ContextCompat.startActivity
 import coil.compose.rememberAsyncImagePainter
 import com.nathaniel.motus.cavevin.R
 import com.nathaniel.motus.cavevin.controller.CellarPictureUtils
 import com.nathaniel.motus.cavevin.controller.CellarStorageUtils
-import com.nathaniel.motus.cavevin.ui.theme.WineCellarMainTheme
+import com.nathaniel.motus.cavevin.data.cellar_database.WineColor
+import com.nathaniel.motus.cavevin.ui.theme.*
 import com.nathaniel.motus.cavevin.viewmodels.BottleDetailViewModel
 
 @OptIn(ExperimentalMaterial3Api::class)
@@ -32,14 +38,25 @@ fun BottleEditContent(
     viewModel: BottleDetailViewModel,
     modifier: Modifier = Modifier
 ) {
-    WineCellarMainTheme {
-        Column {
+    WineCellarMainTheme() {
+        Column() {
             val inputImageName by viewModel.imageName.observeAsState("")
-            Box(modifier = modifier.fillMaxWidth(), contentAlignment = Alignment.Center) {
-                BottleImage(imageName = inputImageName, modifier = modifier)
+            val inputWineColor by viewModel.wineColor.observeAsState("")
+            val inputAppellation by viewModel.appellation.observeAsState("")
+
+            Box(modifier = modifier.fillMaxWidth(), contentAlignment = Alignment.Center)
+            {
+                BottleImage(
+                    imageName = inputImageName,
+                    //imageName = null,
+                    wineColor = inputWineColor,
+                    appellation = inputAppellation,
+                    modifier = modifier.fillMaxWidth()
+                )
             }
 
-            val inputAppellation by viewModel.appellation.observeAsState("")
+            ImagePicker(thereIsAnImage = inputImageName != "", modifier = modifier.fillMaxWidth())
+
             OutlinedTextField(
                 value = inputAppellation,
                 onValueChange = { viewModel.onAppellationChange(it) },
@@ -70,42 +87,100 @@ fun BottleEditContent(
 @Composable
 fun BottleImage(
     imageName: String?,
+    wineColor: String,
+    appellation: String?,
     imageSize: Int = 200,
     imagePadding: Int = 8,
-    modifier: Modifier
+    modifier: Modifier=Modifier
 ) {
     val context = LocalContext.current
-    if (imageName != null)
-        Image(
-            painter = rememberAsyncImagePainter(
-                model = CellarStorageUtils.getBitmapFromInternalStorage(
-                    LocalContext.current.filesDir,
-                    LocalContext.current.resources.getString(R.string.photo_folder_name),
-                    imageName
-                )
-            ), contentDescription = "",
-            contentScale = ContentScale.Fit,
-            modifier = modifier
-                .size(imageSize.dp)
-                .padding(imagePadding.dp)
-                .clickable {
-                    val intent = Intent(Intent.ACTION_VIEW)
-                    intent.setDataAndType(
-                        CellarPictureUtils.getUriFromFileProvider(
-                            context,
-                            imageName!!
-                        ), "image/*"
+    Column() {
+        if (imageName != null)
+            Image(
+                painter = rememberAsyncImagePainter(
+                    model = CellarStorageUtils.getBitmapFromInternalStorage(
+                        LocalContext.current.filesDir,
+                        LocalContext.current.resources.getString(R.string.photo_folder_name),
+                        imageName
                     )
-                    startActivity(context, intent, null)
-                }
-        )
-    else
+                ), contentDescription = "",
+                contentScale = ContentScale.Fit,
+                modifier = modifier
+                    .size(imageSize.dp)
+                    .padding(imagePadding.dp)
+                    .clickable {
+                        val intent = Intent(Intent.ACTION_VIEW)
+                        intent.setDataAndType(
+                            CellarPictureUtils.getUriFromFileProvider(
+                                context,
+                                imageName!!
+                            ), "image/*"
+                        )
+                        startActivity(context, intent, null)
+                    }
+            )
+        else {
+            val placeHolderColor = when (wineColor) {
+                WineColor.RED -> redWineColor
+                WineColor.WHITE -> whiteWhineColor
+                else -> pinkWineColor
+            }
+            val placeHolderName: String =
+                if (appellation != "") appellation?.first().toString() else ""
+            Box(
+                modifier = modifier.requiredSize(imageSize.dp),
+                contentAlignment = Alignment.Center
+            ) {
+                Text(
+                    modifier = modifier
+                        .drawBehind {
+                            drawCircle(
+                                color = placeHolderColor,
+                                radius = this.size.maxDimension
+                            )
+                        },
+                    color = MaterialTheme.colorScheme.onPrimary,
+                    text = placeHolderName,
+                    fontSize = (imageSize / 5).sp
+                )
+            }
+        }
+    }
+}
+
+@Composable
+fun ImagePicker(
+    thereIsAnImage: Boolean,
+    modifier: Modifier=Modifier,
+) {
+    Row(
+        modifier = modifier.fillMaxWidth(),
+        horizontalArrangement = Arrangement.Center
+    ) {
+        if (thereIsAnImage)
+            ImagePickerButton(resourceID = R.drawable.outline_delete_forever_24)
+
+        ImagePickerButton(resourceID = R.drawable.outline_folder_24)
+
+        ImagePickerButton(resourceID = R.drawable.outline_camera_alt_24)
+    }
+}
+
+@Composable
+fun ImagePickerButton(
+    onClick: () -> Unit = {},
+    resourceID: Int,
+    contentDescription: String = "",
+    modifier: Modifier=Modifier
+) {
+    FilledTonalButton(
+        onClick = onClick,
+        modifier = modifier.padding(8.dp)
+    ) {
         Image(
-            //todo add image placeholder
-            painter = painterResource(id = R.drawable.ic_baseline_arrow_back_48), contentDescription = "",
-            contentScale = ContentScale.Fit,
-            modifier = modifier
-                .size(imageSize.dp)
-                .padding(imagePadding.dp)
+            painter = painterResource(id = resourceID),
+            contentDescription = contentDescription,
+            colorFilter = ColorFilter.tint(MaterialTheme.colorScheme.onPrimaryContainer)
         )
+    }
 }
