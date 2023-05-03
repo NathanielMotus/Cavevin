def154da682c465cf095264de6bf68a9811dcabd
Nathaniel Motus <nathaniel.motus@laposte.net>
2023-05-03 21:21:15 +0200

    Create BottleListTopBar.kt

--- app/src/main/java/com/nathaniel/motus/cavevin/ui/bottle_list/BottleListContent.kt
+++ app/src/main/java/com/nathaniel/motus/cavevin/ui/bottle_list/BottleListContent.kt
@@ -3,41 +3,52 @@ package com.nathaniel.motus.cavevin.ui.bottle_list
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.foundation.lazy.items
 import androidx.compose.foundation.rememberScrollState
 import androidx.compose.foundation.verticalScroll
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.getValue
 import androidx.compose.runtime.livedata.observeAsState
 import androidx.compose.ui.Modifier
+import com.nathaniel.motus.cavevin.data.CellarItem
 import com.nathaniel.motus.cavevin.ui.elements.BottleCard
 import com.nathaniel.motus.cavevin.viewmodels.BottleListViewModel
 
 @Composable
 fun BottleListContent(
     viewModel: BottleListViewModel,
-    onNavigateToDetail:(bottleId:Int)->Unit,
+    onNavigateToDetail: (bottleId: Int) -> Unit,
     modifier: Modifier = Modifier,
 ) {
     val cellarItems by viewModel.cellarItems.observeAsState(initial = null)
 
-    LazyColumn(modifier=modifier){
-        cellarItems?.forEach {
-            item { BottleCard(
-                bottleId=it.bottleId,
-                bottleImageBitmap = it.bottleImageBitmap,
-                bottleImageUri = it.bottleImageUri,
-                appellation = it.appellation,
-                domain = it.domain,
-                cuvee = it.cuvee,
-                vintage = it.vintage,
-                bottleTypeAndCapacity = it.bottleTypeAndCapacity,
-                wineColor = it.wineColor,
-                wineStillness = it.wineStillness,
-                rating = it.rating,
-                stock =it.stock,
-                onClick = {onNavigateToDetail(it)},
-                onCountChange={stock:Int->viewModel.updateStockForBottleInCurrentCellar(it.bottleId,stock)}
-            ) }
-        }
+    LazyColumn(modifier = modifier) {
+        if (cellarItems != null)
+            items(
+                items = cellarItems as List<CellarItem>,
+                key = { cellarItem: CellarItem -> cellarItem.bottleId }
+            ) {
+                BottleCard(
+                    bottleId = it.bottleId,
+                    bottleImageBitmap = it.bottleImageBitmap,
+                    bottleImageUri = it.bottleImageUri,
+                    appellation = it.appellation,
+                    domain = it.domain,
+                    cuvee = it.cuvee,
+                    vintage = it.vintage,
+                    bottleTypeAndCapacity = it.bottleTypeAndCapacity,
+                    wineColor = it.wineColor,
+                    wineStillness = it.wineStillness,
+                    rating = it.rating,
+                    stock = it.stock,
+                    onClick = { onNavigateToDetail(it) },
+                    onCountChange = { stock: Int ->
+                        viewModel.updateStockForBottleInCurrentCellar(
+                            it.bottleId,
+                            stock
+                        )
+                    }
+                )
+            }
     }
 }
--- app/src/main/java/com/nathaniel/motus/cavevin/ui/bottle_list/BottleListTopBar.kt
+++ app/src/main/java/com/nathaniel/motus/cavevin/ui/bottle_list/BottleListTopBar.kt
@@ -2,20 +2,34 @@ package com.nathaniel.motus.cavevin.ui.bottle_list
 
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.outlined.Menu
+import androidx.compose.material.icons.outlined.MoreVert
 import androidx.compose.material3.*
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.getValue
 import androidx.compose.runtime.livedata.observeAsState
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.res.painterResource
+import com.nathaniel.motus.cavevin.R
 import com.nathaniel.motus.cavevin.viewmodels.BottleListViewModel
 
 @OptIn(ExperimentalMaterial3Api::class)
 @Composable
 fun BottleListTopBar(viewModel: BottleListViewModel, modifier: Modifier = Modifier) {
     val cellarName by viewModel.cellarName.observeAsState(initial = "")
+    val menuExpanded= remember { mutableStateOf(false) }
     TopAppBar(title = {Text(text = cellarName)},
     navigationIcon = { IconButton(onClick = { /*TODO*/ }) {
         Icon(imageVector = Icons.Outlined.Menu, contentDescription = "")
 
-    }})
+    }},
+    actions = {
+        IconButton(onClick = { /*TODO*/ }) {
+            Icon(painter = painterResource(id = R.drawable.outline_warehouse_24), contentDescription ="" )
+        }
+        IconButton(onClick = { menuExpanded.value=!menuExpanded.value}) {
+            Icon(imageVector = Icons.Outlined.MoreVert, contentDescription ="" )
+        }
+    })
 }
